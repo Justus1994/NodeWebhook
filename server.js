1363a8f39124bfa92012fb5ea3ef2266b6b41b4b
Justus1994 <buybypenny@googlemail.com>
2019-03-19 20:21:42 +0100

    added hook.sh and server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const http    = require('http');
+const spawn   = require('child_process').exec;
+
+
+
+const secret  = 'mytestsecret'; // secret key of the webhook
+const port    = 8081; // port
+
+http.createServer(function(req, res){
+
+  console.log("request received");
+  console.log("running hook.sh");
+
+
+  var deploySh = spawn('sh hook.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
+
+
+
+}).listen(port);
+
+console.log("Server listening at " + port);
